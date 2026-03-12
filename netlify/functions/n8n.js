f3297ca3c5a45e8ea9941c7a409b150b49667cb4
Nexus AI <thony.ferreira.az@gmail.com>
2026-03-12 20:48:54 +0000

    setup n8n netlify

--- netlify/functions/n8n.js
+++ netlify/functions/n8n.js
@@ -1,36 +1,30 @@
 const { spawn } = require("child_process");
 
 let started = false;
-let processRef = null;
 
 exports.handler = async function () {
 
   if (!started) {
 
-    processRef = spawn("npx", ["n8n"], {
+    spawn("npx", ["n8n", "start"], {
       shell: true,
+      stdio: "inherit",
       env: {
         ...process.env,
-        N8N_PORT: "8888",
         N8N_HOST: "0.0.0.0",
+        N8N_PORT: "5678",
         N8N_SECURE_COOKIE: "false"
       }
-    });
-
-    processRef.stdout.on("data", data => {
-      console.log(data.toString());
-    });
-
-    processRef.stderr.on("data", data => {
-      console.log(data.toString());
     });
 
     started = true;
   }
 
   return {
-    statusCode: 200,
-    body: "n8n iniciado"
+    statusCode: 302,
+    headers: {
+      Location: "/webhook"
+    }
   };
 
 };
